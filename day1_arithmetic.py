dca03eaa9970973df7219df2b1546eb4b90a79e0
themanish13 <manishmainali343@gmail.com>
2025-11-23 19:20:24 +0545

    Day1: Arithmetic exercise

---
+++ day1_arithmetic.py
@@ -0,0 +1,14 @@
+a= int(input("Enter first number: "))
+b= int(input("Enter second number: "))
+sum= a + b
+difference= a - b
+product = a * b
+quotient = a / b
+division= a // b
+modulus= a % b
+print("the sum is " + str(sum))
+print("the difference is " + str(difference))
+print("the product is " + str(product))
+print("the quotient is " + str(quotient))
+print("the division is " + str(division))
+print("the modulus is " + str(modulus))
