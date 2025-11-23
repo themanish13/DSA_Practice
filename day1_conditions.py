6818b3d48d9dea74ca343b460af7dc4e645458d2
themanish13 <manishmainali343@gmail.com>
2025-11-23 19:20:31 +0545

    Day1: Conditions exercise

---
+++ day1_conditions.py
@@ -0,0 +1,7 @@
+balls= int(input ("Enter how many balls you got: "))
+if balls ==2:
+    print("You have got two balls and you are a man!")
+elif balls <2:
+    print("You have less balls  and you are a gay man!. sorry for your loss")
+else:
+    print("You are gay but you have more balls than man bro be proud and loud!")
